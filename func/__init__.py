fce93082ec792f2f1ed1cf37409ed1adb5a704d2
wangkai <wangkai@utopilot.ai>
2024-03-18 17:18:49 +0800

    update

--- func/__init__.py
+++ func/__init__.py
@@ -2,18 +2,20 @@ data1 = ['它对这个世界的规律有了一个', '比较更为完整的了解
 data2 = ['最神奇的是他把这些事物之间的相互关系', '这些物理规律好像都能学习', '这个物理规律好像都能学习进去。']
 
 
-def merge_sentences(sentences, num=3, ratio_t=0.8):
+def merge_sentences(sentences, num=3, similar=0.8):
+    indices = []
+    size = len(sentences)
+    for i in range(size):
+        sen_i = sentences[i]
+        for j in range(i + 1, min(i + num, size)):
+            sen_j = sentences[j]
+            if len(set(sen_i) & set(sen_j)) / min(len(set(sen_i)), len(set(sen_j))) > similar:
+                index = [i, j].index()
 
-    remove_indices = []
-    for i in range(len(sentences)):
-        sen1 = sentences[i]
-        for j in range(i + 1, min(i + num, len(sentences))):
-            sen2 = sentences[j]
-            ratio = len(''.join(set(sen1) & set(sen2))) / min(len(sen1), len(sen2))
+            # sen2 = sentences[j]
+            # ratio = len(''.join(set(sen1) & set(sen2))) / min(len(sen1), len(sen2))
+            #
+            # print(i, j, ratio)
 
 
-
-            print(i, j, ratio)
-
-
-merge_sentences(data1)
+merge_sentences(data2)
